047b1358f46f90214f61240397126e587a719a9e
Santosh <santoshkumar.kollati@gmail.com>
2025-04-21 12:37:06 -0600

    output sample file

---
+++ 02-outputs/output.tf
@@ -0,0 +1,9 @@
+output "sample" {
+    value = "Hello, World!"
+    description = "This is a sample output variable."
+}
+
+output "name" {
+    value = 1000
+    description = "This is a sample output number."
+}
