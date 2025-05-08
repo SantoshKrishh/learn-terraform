c1a7bc49118b47c310ae89ff9dcec164ec8b5dd3
Santosh <santoshkumar.kollati@gmail.com>
2025-05-08 04:09:08 -0600

    ami id test

--- 04-datasources/data.tf
+++ 04-datasources/data.tf
@@ -1,6 +1,6 @@
 data "aws_ec2_spot_price" "example" {
   instance_type     = "t3.medium"
-  availability_zone = "us-east-1a"
+  availability_zone = "us-east-1a" 
 
   filter {
     name   = "product-description"
@@ -10,4 +10,11 @@ data "aws_ec2_spot_price" "example" {
 
 output "example" {
   value = data.aws_ec2_spot_price.example.spot_price
+}
+
+data "aws_ami" "example" {
+  executable_users = ["self"]
+  most_recent      = true
+  name_regex       = "devops-practice"
+  owners           = ["self"]
 }
