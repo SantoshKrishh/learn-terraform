8869b123a251748a35c5ac1207ee9dc92974027f
Santosh <santoshkumar.kollati@gmail.com>
2025-05-08 04:56:36 -0600

    ami id test

--- 04-datasources/data.tf
+++ 04-datasources/data.tf
@@ -28,7 +28,6 @@ output "securitygroup" {
 
 
 data "aws_ami" "test" {
-  executable_users = ["self"]
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
   owners           = ["973714476881"]
