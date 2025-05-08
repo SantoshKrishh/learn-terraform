915f0091593572b397cc1584e97846ac4ee921ec
Santosh <santoshkumar.kollati@gmail.com>
2025-05-08 04:14:12 -0600

    test

--- 04-datasources/data.tf
+++ 04-datasources/data.tf
@@ -1,4 +1,4 @@
-data "aws_ec2_spot_price" "example" {
+data "aws_ec2_spot_price" "spotprice" {
   instance_type     = "t3.medium"
   availability_zone = "us-east-1a" 
 
@@ -8,13 +8,18 @@ data "aws_ec2_spot_price" "example" {
   }
 }
 
-output "example" {
-  value = data.aws_ec2_spot_price.example.spot_price
+output "spot_price" {
+  value = data.aws_ec2_spot_price.spotprice
 }
 
-data "aws_ami" "example" {
+data "aws_ami" "ami_id" {
   executable_users = ["self"]
   most_recent      = true
   name_regex       = "devops-practice"
   owners           = ["self"]
+}
+
+output "ami_id" {
+  value = data.aws_ami.ami_id.id
+  
 }
