9f530f02c5c69ebd46d5122d8136485e372af951
Santosh <santoshkumar.kollati@gmail.com>
2025-05-26 12:38:27 -0600

    loops concept

---
+++ 10-Loops/count/main.tf
@@ -0,0 +1,19 @@
+data "aws_ami" "test" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
+}
+
+resource "aws_instance" "web" {
+  count = length(var.instances)
+  ami           = data.aws_ami.test.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+variable "instances" {
+  default = ["frontend", "catalogue", "cart"]
+}
