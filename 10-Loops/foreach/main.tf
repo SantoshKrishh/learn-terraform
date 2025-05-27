286883fbe6ef42ebc93c4fbbe73ade5ea5cdc11a
Santosh <santoshkumar.kollati@gmail.com>
2025-05-27 01:19:06 -0600

    for each loop

--- 10-Loops/foreach/main.tf
+++ 10-Loops/foreach/main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "web" {
   for_each = var.instances
     # Using for_each instead of count
   ami           = data.aws_ami.test.id
-  instance_type = lookup(each.value, "instance_type", "t3.micro")
+  instance_type = lookup(each.value, "instance_type", "t3.small")
 
   tags = {
     Name = each.key
@@ -20,15 +20,14 @@ variable "instances" {
     default = {
         frontend = {
             Name = "frontend"
-            instance_type = "t3.micro"
         },
         catalogue = {
             Name = "catalogue"
-            instance_type = "t3.micro"
+            instance_type = "t3.nano"
         },
         cart = {
             Name = "cart"
-            instance_type = "t3.micro"
+            instance_type = "t3.nano"
         }
     }
 }
